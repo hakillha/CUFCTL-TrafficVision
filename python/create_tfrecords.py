629b07c8f1e7cbf8ce2ffd1de8380c8542a85f43
hakillha <yingges@clemson.edu>
2018-05-10 16:11:31 -0400

    updates

--- python/create_tfrecords.py
+++ python/create_tfrecords.py
@@ -6,12 +6,15 @@ from modules.utils.data_utils import create_labelmap, create_trainval_set, gen_t
 def parse_args():
 	parser = argparse.ArgumentParser(description='')
 	parser.add_argument('--in_path',
-						dest='in_path', help='')
+						dest='in_path', 
+						help='The folder that contains \'Insight-MVT_Annotation_Train\'.')
 	parser.add_argument('--out_path',
+						default='data/tfrecords',
 						dest='out_path', help='')
 	parser.add_argument('--label_map_path',
+						default='data/ua_detrac_labelmap.pbtxt',
 						dest='label_map_path',
-						help='Label map output path.')
+						help='Specify input label map file path.')
 	parser.add_argument('--occ_ratio_threshold', 
 						default=0.4,
 						dest='occ_ratio_threshold', 
@@ -35,4 +38,6 @@ labels = ['car', 'bus', 'van', 'others']
 create_labelmap(labels)
 # create_trainval_set(args.in_path, train=0.8, val=0.2)
 
-gen_tfrecords(args.in_path, train=args.train, val=args.val, occ_ratio_threshold=args.occ_ratio_threshold)
+gen_tfrecords(args.in_path, train=args.train, val=args.val,
+			  out_path=args.out_path, label_map_path=args.label_map_path,
+			  occ_ratio_threshold=args.occ_ratio_threshold)
